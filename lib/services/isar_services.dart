e0d7b475965c6ec253ee87a1e1c0a4b07625210a
Abderrahmane <abderrahmanekihal5@gmail.com>
2023-12-09 02:10:30 +0100

    update isarServices.dart

--- lib/services/isar_services.dart
+++ lib/services/isar_services.dart
@@ -9,55 +9,21 @@ class IsarService {
     db = openDB();
   }
 
-  /* Future<void> saveCourse(Course newCourse) async {
+  Future<void> saveRecord(RecordModel newRecord) async {
     final isar = await db;
-    isar.writeTxnSync<int>(() => isar.courses.putSync(newCourse));
+    isar.writeTxnSync<int>(() => isar.recordModels.putSync(newRecord));
   }
 
-  Future<void> saveStudent(Student newStudent) async {
+  Future<List<RecordModel>> getAllRecords() async {
     final isar = await db;
-    isar.writeTxnSync<int>(() => isar.students.putSync(newStudent));
+    return await isar.recordModels.where().findAll();
   }
 
-  Future<void> saveTeacher(Teacher newTeacher) async {
+  Stream<List<RecordModel>> listenToCourses() async* {
     final isar = await db;
-    isar.writeTxnSync<int>(() => isar.teachers.putSync(newTeacher));
+    yield* isar.recordModels.where().watch(fireImmediately: true);
   }
 
-  Future<List<Course>> getAllCourses() async {
-    final isar = await db;
-    return await isar.courses.where().findAll();
-  }
-
-  Stream<List<Course>> listenToCourses() async* {
-    final isar = await db;
-    yield* isar.courses.where().watch(initialReturn: true);
-  }
-
-  Future<void> cleanDb() async {
-    final isar = await db;
-    await isar.writeTxn(() => isar.clear());
-  }
-
-  Future<List<Student>> getStudentsFor(Course course) async {
-    final isar = await db;
-    return await isar.students
-        .filter()
-        .courses((q) => q.idEqualTo(course.id))
-        .findAll();
-  }
-
-  Future<Teacher?> getTeacherFor(Course course) async {
-    final isar = await db;
-
-    final teacher = await isar.teachers
-        .filter()
-        .course((q) => q.idEqualTo(course.id))
-        .findFirst();
-
-    return teacher;
-  }
-*/
   Future<void> cleanDb() async {
     final isar = await db;
     await isar.writeTxn(() => isar.clear());
